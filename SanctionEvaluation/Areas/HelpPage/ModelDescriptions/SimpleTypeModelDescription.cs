a41a1b2633437137e15737d081293e4b108d6d51
jlb091 <Boehler091@gmail.com>
2017-06-08 21:03:07 -0500

    Add project files.

---
+++ SanctionEvaluation/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SanctionEvaluation.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Sanctions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sanctions.MvcApplication" Language="C#" %>
